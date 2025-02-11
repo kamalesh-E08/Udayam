b2642a1a002abe8005dd9dd4410a7bfc64a839d1
kamalesh-E08 <kamaleshe76@gmail.com>
2025-02-11 14:38:36 +0530

    footer commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2025 Udhayam Cultural Fest | Designed by CSBS Department</p>
+      <p>&copy; 2025 Udhayam Cultural Fest | Designed by <a href = "https://www.instagram.com/__kamal__.08/">Kamalesh (CSBS)</a> | <a href='https://www.instagram.com/mr_v3_moto_holic_/'>Kamesh(CSBS)</a></p>
     </footer>
   );
 };
