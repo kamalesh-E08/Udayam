98689574793a37bc559b916065fd396d8a5a22dd
kamalesh-E08 <kamaleshe76@gmail.com>
2025-02-11 14:03:49 +0530

    font changes

--- src/Components/LoadingPage.jsx
+++ src/Components/LoadingPage.jsx
@@ -5,7 +5,7 @@ import loading from "../assets/Loading.mp4"
 export default function LoadingPage() {
   return (
     <div className="loading">
-      <video autoPlay muted loop alt="Cultural Event" className="loading-image">
+      <video autoPlay muted loop playsInline alt="Cultural Event" className="loading-image">
         <source src={loading} type = "video/mp4"></source>
       </video>
     </div>
