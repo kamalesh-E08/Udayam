96481d56e5676ddc5567431698e7f3a05527e552
kamalesh-E08 <kamaleshe76@gmail.com>
2025-03-03 14:53:39 +0530

    final Commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,23 +4,12 @@ import gifimage from '../../public/gifimage.gif';
 
 const Home = () => {
   return (
-<<<<<<< HEAD
-    // <section id="home">
-    <section id="home" style={{ backgroundImage: `url(${gifimage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div className="home-container">
-        <h2 className="home-heading">Welcome to the  Cultural Fest Of KIT.!!</h2>
-        <img src={cultural} alt="Cultural Program Poster" className="home-poster" />
-        
-      </div>
-=======
-    <section id="home">
-      <h2>Welcome to the Udhayam Fest</h2>
-      <img src={cultural}  alt="Cultural Program Poster" className="poster" />
-      <div className="scroll-content">
-        <p>Join us for a series of exciting events and activities that showcase the talents of our students.</p>
-      </div>    
->>>>>>> 9c5bff0 (font changes)
-    </section>
+      <section id="home" style={{ backgroundImage: `url(${gifimage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+        <div className="home-container">
+          <h2 className="home-heading">Welcome to the  Cultural Fest Of KIT.!!</h2>
+          <img src={cultural} alt="Cultural Program Poster" className="home-poster" />
+        </div>
+      </section>
   );
 };
 
